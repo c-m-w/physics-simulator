e02547e62350bc747a5b86e333552bb706aacadb
c-m-w <c-m-w-other@tuta.io>
2022-12-21 13:59:48 -0700

    cleanup

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -1,21 +1,9 @@
 /// MainHeader.js
 
-import {useState} from "react";
-
 import useUser from "../hooks/useUser";
-
-import LoginPopup from "./LoginPopup";
-import RegisterPopup from "./RegisterPopup";
-
-const Popup = {
-    None: 0,
-    Login: 1,
-    Register: 2
-}
 
 export default function MainHeader() {
 
-    const [popup, setPopup] = useState(Popup.None);
     const user = useUser();
 
     const userText =
@@ -35,18 +23,6 @@ export default function MainHeader() {
                 {userText}
                 <h1 className="white centered">physics simulator</h1>
             </header>
-            <div className="actions">
-                <button id="enter">enter</button>
-                <button 
-                    disabled={user.isLoggedIn()}
-                    onClick={() => setPopup(Popup.Login)} 
-                    id="login-popup">login</button>
-                <button disabled={user.isLoggedIn()}
-                    onClick={() => setPopup(Popup.Register)} 
-                    id="register-popup">register</button>
-            </div>
-            {popup == Popup.Login && <LoginPopup closePopup={() => setPopup(Popup.None)} />}
-            {popup == Popup.Register && <RegisterPopup closePopup={() => setPopup(Popup.None)} />}
         </>
     );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,39 @@
 /// Home.js
 
+import {useState} from "react";
+
+import useUser from "../hooks/useUser";
+
 import MainHeader from "../components/MainHeader";
+import LoginPopup from "../components/LoginPopup";
+import RegisterPopup from "../components/RegisterPopup";
+
+const Popup = {
+    None: 0,
+    Login: 1,
+    Register: 2
+}
 
 export default function Home() {
+
+    const [popup, setPopup] = useState(Popup.None);
+    const user = useUser();
 
     return (
         <>
             <MainHeader />
+            <div className="actions">
+                <button id="enter">enter</button>
+                <button 
+                    disabled={user.isLoggedIn()}
+                    onClick={() => setPopup(Popup.Login)} 
+                    id="login-popup">login</button>
+                <button disabled={user.isLoggedIn()}
+                    onClick={() => setPopup(Popup.Register)} 
+                    id="register-popup">register</button>
+            </div>
+            {popup == Popup.Login && <LoginPopup closePopup={() => setPopup(Popup.None)} />}
+            {popup == Popup.Register && <RegisterPopup closePopup={() => setPopup(Popup.None)} />}
         </>
     )
 }
