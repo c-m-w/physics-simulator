50208e2789edc656077aa724b128d8a9b0a74883
c-m-w <114171277+mariat798@users.noreply.github.com>
2023-01-22 19:07:45 -0700

    Added routes

--- backend/server.py
+++ backend/server.py
@@ -64,10 +64,54 @@ def login():
 
     return make_response(False, "bad password")
 
-# level/id PUT (save level)
-# level/id GET (load level)
-# level POST (create level)
-# levels/email (get level list)
+@app.route("/api/level/<int:id>", methods=["GET", "PUT"])
+def level(id):
+
+    if request.method == 'GET':
+
+        query = db.session.execute(db.select(Level)
+                                    .where(Level.id==id)).scalars().one()
+
+        return make_response(True, data={"data": query.data, "id": query.id, "name": query.name})
+    
+    db.session.execute(db.update(Level).where(Level.id==id).values(**request.json))
+    db.session.commit()
+        
+    return make_response(True)
+
+
+@app.route("/api/level" , methods=["POST"])
+def post_level():
+
+    print(request.json)
+
+    user_id = db.session.execute(db.select(User).where(User.email==request.json["email"])).scalars().one().id
+
+    db.session.add(Level(user_id=user_id,
+                         name=request.json["name"],
+                         data='{"objects":[],"fields":[]}'))
+    db.session.commit()
+
+    last = db.session.query(Level).order_by(Level.id.desc()).first()
+
+    return make_response(True, data={"id": last.id, "name": last.name, "data": last.data})
+
+
+@app.route("/api/levels/<string:email>")
+def get_level_list(email):
+    
+    user_id = db.session.execute(db.select(User).where(User.email==email)).scalars().one().id
+    query = db.session.execute(db.select(Level).where(Level.user_id == user_id)).scalars()
+
+    data = []
+
+    for level in query:
+
+        data.append({"data": level.data, "id": level.id, "name": level.name})
+
+    return make_response(True, data=data)
+
+# /api/levels/email (get level list)
 
 if __name__ == "__main__":
 
